8ee1c092b383e36facf17f5d8ace97e8e29ed33f
Bonfanti Cecilia <bonfanticeciliaemilce@gmail.com>
2023-11-19 17:15:54 -0300

    Se agrega Tabs de bootstrap y se estiliza

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,6 +1,9 @@
 import { MyNavbar } from "../components/Navbar";
 import { Footer } from "../components/Footer";
+import Tab from 'react-bootstrap/Tab';
+import Tabs from 'react-bootstrap/Tabs';
 import '../styles/profile.css'
+
 
 export function Profile() {
   return (
@@ -14,7 +17,21 @@ export function Profile() {
           <h1 className="title">Mi cuenta</h1>
           <p>Modifica tus datos de cuenta y contacto</p>
       </banner>
-      <div className="card-user mx-auto"></div>
+      <div className="card-user mx-auto">
+        <Tabs
+        defaultActiveKey="profile"
+        id="fill-tab-example"
+        className="mb-3"
+        fill
+      >
+        <Tab eventKey="info-user" title="Información personal">
+          Tab content for Home
+        </Tab>
+        <Tab eventKey="password" title="Contraseña">
+          Tab content for Loooonger Tab
+        </Tab>
+      </Tabs>
+      </div>
       <Footer/>
     </main>
   );
