895442594827d7eee49c15d7d5e86d2e09270dc7
Bonfanti Cecilia <bonfanticeciliaemilce@gmail.com>
2023-11-19 16:40:45 -0300

    Iniciada la vista profile, se agrega bg, titulos, nav y footer

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,3 +1,22 @@
+import { MyNavbar } from "../components/Navbar";
+import { Footer } from "../components/Footer";
+import '../styles/profile.css'
+
 export function Profile() {
-  return <h1>Mi Cuenta</h1>;
-}
+  return (
+    <main>
+      <MyNavbar/>
+      <banner className='banner'>
+        <img
+            src="/banner.png"
+            alt=""
+          />
+          <h1 className="title">Mi cuenta</h1>
+          <p>Modifica tus datos de cuenta y contacto</p>
+      </banner>
+      <div className="card-user mx-auto"></div>
+      <Footer/>
+    </main>
+  );
+  }
+
