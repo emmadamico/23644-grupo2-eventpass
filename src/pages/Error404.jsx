6273772f9e1119fdd997098947c282f5c8812048
Marcelo Melogno <marcelomelogno1986@gmail.com>
2023-11-19 22:07:51 -0300

    Estilos pagina error404

--- src/pages/Error404.jsx
+++ src/pages/Error404.jsx
@@ -1,3 +1,22 @@
+import { Link } from "react-router-dom";
+
 export function NotFound() {
-  return <h1>Error 404 | Not Found</h1>;
+  return (
+    <>
+      <section className="vh-100 d-flex flex-column align-items-center justify-content-center gap-5">
+        <article className="d-flex gap-3 align-items-center justify-content-center">
+          <div className="display-1 border-1 border-end border-dark px-4 text-white">
+            404
+          </div>
+          <div className="fs-2 versalita">Not Found</div>
+        </article>
+        <article className="d-flex align-items-center gap-5">
+          <p className="m-0 p-0 text-white">Página no Encontrada</p>
+          <Link to={"/"} className="hover">
+            Inicio
+          </Link>
+        </article>
+      </section>
+    </>
+  );
 }
