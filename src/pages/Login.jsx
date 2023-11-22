5557d981122bb1b3b0c479a37ecd02c8f234558c
Daniela Homobono <danielahomobono81@gmail.com>
2023-11-22 19:08:46 -0300

    Actualización de vista Login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,46 +1,57 @@
 import React from 'react';
-import { Form, Button } from 'react-bootstrap';
+import { Form, Button, Row, Col } from 'react-bootstrap';
 import "../App.css";
 import "../styles/Login.css";
 import { MyNavbar } from '../components/Navbar';
 import { Footer } from '../components/Footer';
 
 export function Login() {
-    return (
-      <div>
+  return (
+    <div>
       <MyNavbar />
-     
-           <div className="container mx-auto">           
+      <div className="body-container">
+      <div className="form-container mx-auto flex-column justify-content-center align-items-center mt-5">
         <div className="row justify-content-center align-items-center">
-          <div className="col-md-6"> 
-            <div className="card p-5" style={{ borderRadius: '1rem' }}> 
-              <div className="card-body">              
-                <h3 className="text-center mb-4">INGRESAR A MI CUENTA</h3>
+          <div className="col-md-8">
+            <div className="card p-7" style={{ borderRadius: '1rem', textAlign: 'center' }}>
+              <div className="card-body">
+                <h3 className="mb-4">INGRESAR A MI CUENTA</h3>
                 <Form>
-                  <Form.Group controlId="formBasicEmail" className="mb-3">
-                    <Form.Label className="mb-0">E-mail</Form.Label>
-                    <Form.Control type="email" placeholder="Ingresa tu correo electrónico" className="py-2" /> {/* Añade padding vertical al campo del formulario */}
+                  <Form.Group as={Row} className="mb-3">
+                    <Form.Label column sm="3">
+                      E-mail
+                    </Form.Label>
+                    <Col sm="9">
+                      <Form.Control type="email" placeholder="Ingresa tu correo electrónico" className="py-2" style={{ borderRadius: '50px' }}/>
+                    </Col>
                   </Form.Group>
-  
-                  <Form.Group controlId="formBasicPassword" className="mb-3">
-                    <Form.Label className="mb-0">Contraseña</Form.Label>
-                    <Form.Control type="password" placeholder="Ingresa tu contraseña" className="py-2" /> 
-                    </Form.Group>
-  
-                  <Form.Group controlId="formBasicCheckbox" className="mb-3">
-                    <Form.Check type="checkbox" label="Recordarme" />
-                  </Form.Group>
-  
-                  <Button variant="primary" type="submit" block className="py-2"> 
-                    Iniciar sesión
+
+                  <Form.Group as={Row} className="mb-3">
+                    <Form.Label column sm="3">
+                      Contraseña
+                    </Form.Label>
+                    <Col sm="9">
+                      <Form.Control type="password" placeholder="Ingresa tu contraseña" className="py-2"style={{ borderRadius: '50px' }} />
+                    </Col>
+                  </Form.Group>                
+                  <Row className="mb-3 align-items-center">
+                 <Col>
+                  <Button variant="light" type="submit" className="w-100 py-2" style={{ borderRadius: '50px' }}>
+                   Ingresar
                   </Button>
+                 </Col>
+                 <Col className="d-flex align-items-center">
+                   <Form.Group controlId="formBasicCheckbox" className="mb-0">
+                   <Form.Check type="checkbox" label="Recordarme" />
+                  </Form.Group>
+                </Col>
+                  </Row>                  
                 </Form>
+                <div className="row mt-3 d-flex justify-content-center">
+  
                 <div className="row mt-3">
-                  <div className="col-md-6">
+                  
                     <a href="#">¿Olvidaste tu contraseña?</a>
-                  </div>
-                  <div className="col-md-6 text-right">
-                    <a href="#">Registrarse</a>
                   </div>
                 </div>
               </div>
@@ -48,8 +59,9 @@ export function Login() {
           </div>
         </div>
       </div>
-      
+     </div>
       <Footer />
-      </div>
-    );
-  }
+    </div>
+  );
+}
+
