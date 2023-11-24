3712acc71f3e8ee9b716edb34ab4c09b47063402
fcg77 <castrofabian@gmail.com>
2023-11-23 22:48:03 -0300

    Revert "Rama para about us en rama Desarrollo"
    This reverts commit 3d387ef83cc28882ef3ef01a55782de8352a1e68.

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,27 +1,7 @@
-import "../App.css";
-import '../styles/faqs.css'
-import Accordion from 'react-bootstrap/Accordion';
-import { MyNavbar } from "../components/Navbar";
-import { Footer } from "../components/Footer";
-
 export function About() {
   return (
-    <main>
-      <MyNavbar />
-      <banner className='banner-faq'>
-        <img
-          src="/banner.png"
-          alt=""
-        />
-        <h1 className="title">About Us</h1>
-      </banner>
-
-      <section className="aboutus">
-        <div className="card-faq mx-auto">
-          
-        </div>
-      </section>
-      <Footer />
-    </main>
+    <>
+      <h1>About Us</h1>
+    </>
   );
 }
