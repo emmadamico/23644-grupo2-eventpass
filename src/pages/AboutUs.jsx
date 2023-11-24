3d387ef83cc28882ef3ef01a55782de8352a1e68
fcg77 <castrofabian@gmail.com>
2023-11-23 22:40:15 -0300

    Rama para about us en rama Desarrollo

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,7 +1,27 @@
+import "../App.css";
+import '../styles/faqs.css'
+import Accordion from 'react-bootstrap/Accordion';
+import { MyNavbar } from "../components/Navbar";
+import { Footer } from "../components/Footer";
+
 export function About() {
   return (
-    <>
-      <h1>About Us</h1>
-    </>
+    <main>
+      <MyNavbar />
+      <banner className='banner-faq'>
+        <img
+          src="/banner.png"
+          alt=""
+        />
+        <h1 className="title">About Us</h1>
+      </banner>
+
+      <section className="aboutus">
+        <div className="card-faq mx-auto">
+          
+        </div>
+      </section>
+      <Footer />
+    </main>
   );
 }
