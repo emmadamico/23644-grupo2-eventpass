79527ad0846eca16a0bd8c5a3265dd92599c6759
Marcelo Melogno <marcelomelogno1986@gmail.com>
2023-11-25 02:36:55 -0300

    Componente SportsEvents incompleto

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,20 +1,13 @@
-
-import React, { useState } from 'react';
-import { Form, Button, Row, Col } from 'react-bootstrap';
+import React, { useState } from "react";
+import { Form, Button, Row, Col } from "react-bootstrap";
 import "../App.css";
 import "../styles/Login.css";
-import { MyNavbar } from '../components/Navbar';
-import { Footer } from '../components/Footer';
-
-
+import { MyNavbar } from "../components/Navbar";
+import { Footer } from "../components/Footer";
 
 export function Login() {
-
-
-  const [formData, setFormData] = useState({ email: '', password: '' });
+  const [formData, setFormData] = useState({ email: "", password: "" });
   const [isLoggedIn, setLoggedIn] = useState(false);
-
-
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
@@ -22,76 +15,94 @@ export function Login() {
   };
 
   const handleLogin = () => {
-    
     if (formData.email && formData.password) {
       setLoggedIn(true);
     } else {
-      alert('Please, enter your email and password');
+      alert("Please, enter your email and password");
     }
   };
-
 
   return (
     <div>
       <MyNavbar />
       <div className="body-container">
-      <div className="form-login form-container mx-auto flex-column justify-content-center align-items-center mt-5">
-        <div className="row justify-content-center align-items-center">
-          <div className="col-md-8">
-            <div className="cardgral card p-7" style={{ borderRadius: '1rem', textAlign: 'center', maxWidth: '600px', margin: 'auto' }}>
-              <div className="card-body">
-                <h3 className="mb-4">LOGIN IN TO MY ACCOUNT</h3>
-                <Form>
-                  <Form.Group as={Row} className="mb-3">
-                    <Form.Label column sm="3">
-                      E-mail
-                    </Form.Label>
-                    <Col sm="9">
-                      <Form.Control type="email" placeholder="Enter your email" className="py-2" style={{ borderRadius: '50px' }}/>
-                    </Col>
-                  </Form.Group>
+        <div className="form-login form-container mx-auto flex-column justify-content-center align-items-center mt-5">
+          <div className="row justify-content-center align-items-center">
+            <div className="col-md-8">
+              <div
+                className="cardgral card p-7"
+                style={{
+                  borderRadius: "1rem",
+                  textAlign: "center",
+                  maxWidth: "600px",
+                  margin: "auto",
+                }}
+              >
+                <div className="card-body">
+                  <h3 className="mb-4">LOGIN IN TO MY ACCOUNT</h3>
+                  <Form>
+                    <Form.Group as={Row} className="mb-3">
+                      <Form.Label column sm="3">
+                        E-mail
+                      </Form.Label>
+                      <Col sm="9">
+                        <Form.Control
+                          type="email"
+                          placeholder="Enter your email"
+                          className="py-2"
+                          style={{ borderRadius: "50px" }}
+                        />
+                      </Col>
+                    </Form.Group>
 
-                  <Form.Group as={Row} className="mb-3">
-                    <Form.Label column sm="3">
-                      Password
-                    </Form.Label>
-                    <Col sm="9">
-                      
-                      <Form.Control type="password" placeholder="Enter your password" className="py-2"style={{ borderRadius: '50px' }} />
-                    </Col>
-                  </Form.Group>                
-                  <Row className="mb-3 align-items-center">
-                 <Col>
-                  <Button variant="light" type="submit" className="w-100 py-2" style={{ borderRadius: '50px' }}onClick={handleLogin}>
-                   Login
-                   </Button>
-                    </Col>    
-                    
-                 
-                 <Col className="d-flex align-items-center">
-                   <Form.Group controlId="formBasicCheckbox" className="mb-0">
-                   <Form.Check type="checkbox" label="Remind me" />
-                  </Form.Group>
-                </Col>
-                </Row>                 
-                </Form>
-                <div className="row mt-3 d-flex justify-content-center">
-  
-                <div className="row mt-3">
-                  
-                    <a href="#">Did you forget your password?</a>
+                    <Form.Group as={Row} className="mb-3">
+                      <Form.Label column sm="3">
+                        Password
+                      </Form.Label>
+                      <Col sm="9">
+                        <Form.Control
+                          type="password"
+                          placeholder="Enter your password"
+                          className="py-2"
+                          style={{ borderRadius: "50px" }}
+                        />
+                      </Col>
+                    </Form.Group>
+                    <Row className="mb-3 align-items-center">
+                      <Col>
+                        <Button
+                          variant="light"
+                          type="submit"
+                          className="w-100 py-2"
+                          style={{ borderRadius: "50px" }}
+                          onClick={handleLogin}
+                        >
+                          Login
+                        </Button>
+                      </Col>
+
+                      <Col className="d-flex align-items-center">
+                        <Form.Group
+                          controlId="formBasicCheckbox"
+                          className="mb-0"
+                        >
+                          <Form.Check type="checkbox" label="Remind me" />
+                        </Form.Group>
+                      </Col>
+                    </Row>
+                  </Form>
+                  <div className="row mt-3 d-flex justify-content-center">
+                    <div className="row mt-3">
+                      <a href="#">Did you forget your password?</a>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-          </div>
-     </div>
-</div>
+        </div>
+      </div>
       <Footer />
     </div>
   );
 }
-
-
-
