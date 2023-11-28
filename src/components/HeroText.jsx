79ecd9d0bd271b7aab619aa5bcb6fad6d4741fac
Marcelo Melogno <117491210+MarceM1@users.noreply.github.com>
2023-11-28 02:47:49 -0300

    Home/component (#39)
    * continuo diseño de home
    
    * Compoenente MusicEvents Completo, Compoenente SportsEvents y FilterSports incompletos
    
    * Correccion importacion de CSS
    
    * Correcion estilos
    
    * Componente SportsEvents incompleto
    
    * Componente SportsEvents al 80%. Correcion responsive y algunos estilos generales
    
    * Componente SportsEvents al 90%
    
    * correccion button
    
    * Commit para probar codigo
    
    * Compoennte  SportEvents completo

--- src/components/HeroText.jsx
+++ src/components/HeroText.jsx
@@ -2,8 +2,8 @@ import "../styles/HeroText.css";
 export function HeroText() {
   return (
     <>
-      <section className="p-0 m-0 hero__text-wrapper d-flex align-items-center glass-bg">
-        <article className="row m-0 p-0 w-100  hero__text-hover text-bigger">
+      <section className="p-0 m-0 hero__text-wrapper d-flex align-items-center">
+        <article className="row m-0 p-0 w-100  hero__text-hover text-bigger glass-bg">
           <div className="col-12 col-lg-5 p-0 m-0 ">
             <h2 className="p-0 m-0 display-1 fw-bold text-center text-lg-end hero__text-green fst-italic textShadow animate__animated animate__bounce">
               <span>Feel</span>
@@ -13,9 +13,11 @@ export function HeroText() {
             <p className="m-0 p-0 px-md-3 px-0 fs-4 text-white text-center text-lg-start textShadow animate__animated animate__zoomIn">
               the moment,
             </p>
-            <p className="m-0 p-0  fs-4 text-center text-lg-end text-white animate__animated animate__bounce">
-              <span className="hero__text-green fst-italic fw-bold textShadow">enjoy </span>{" "}
-               the
+            <p className="m-0 p-0  fs-4 text-center text-lg-end text-white animate__animated animate__bounce animate__animated animate__bounce">
+              <span className="hero__text-green fst-italic fw-bold textShadow textShadow">
+                enjoy{" "}
+              </span>{" "}
+              the
             </p>
           </div>
           <div className="col-12 col-lg-5 p-0 m-0 ">
@@ -23,10 +25,8 @@ export function HeroText() {
               Life
             </h2>
           </div>
-        </article>        
+        </article>
       </section>
     </>
   );
 }
-
-
--- src/components/SportsEvents.jsx
+++ src/components/SportsEvents.jsx
@@ -9,48 +9,75 @@ import { PopularSports } from "./PopularSports";
 import "../styles/SportsEvents.css";
 
 export function SportsEvents() {
-  const elementos = 1;
-  const [page, setPage] = useState(1);
-  const [selectedSegmentId, setSelectedSegmentId] = useState(null);
+  const elementos = 200;
+  const [index, setIndex] = useState(0);
+  const [filteredEvents, setFilteredEvents] = useState([]);
+  const [restEvents, setRestEvents] = useState([]);
+  const selectedSegmentId = "KZFzniwnSyZfZ7v7nJ";
+  const [genreName, setGenreName] = useState("Basketball");
+
+  const handleGenreName = (key) => {
+    setGenreName(key);
+    console.log(key);
+  };
 
   const handleIncrementPage = () => {
-    setPage(page + 1);
+    setIndex((prevIndex) =>
+      prevIndex < filteredEvents.length - 1 ? prevIndex + 1 : prevIndex
+    );
   };
 
   const handleDecrementPage = () => {
-    setPage(page > 1 ? page - 1 : page);
+    setIndex((prevIndex) => (prevIndex > 0 ? prevIndex - 1 : prevIndex));
   };
 
-  const handleSegmentClick = (segmentId) => {
-    setSelectedSegmentId(segmentId);
-    console.log("Segment ID seleccionado:", segmentId);
-  };
-  const url = `${process.env.REACT_APP_URL}${process.env.REACT_APP_CONSUMER_KEY}&page=${page}&size=${elementos}&segmentId=KZFzniwnSyZfZ7v7nJ`;
+  const url = `${process.env.REACT_APP_URL}${process.env.REACT_APP_CONSUMER_KEY}&size=${elementos}&segmentId=KZFzniwnSyZfZ7v7nE`;
 
-  let { data, isPending, error, performFetch } = useFetch(url);
+  let {
+    data: events,
+    isPending,
+    error,
+    performFetch,
+  } = useFetch(url, [genreName]);
+  console.log(isPending, error);
+  console.log("Events:", events);
+
   useEffect(() => {
-    const fetchData = async () => {
-      let newUrl = `${process.env.REACT_APP_URL}${process.env.REACT_APP_CONSUMER_KEY}&page=${page}&size=${elementos}`;
+    const genreNamesSet = new Set();
 
-      if (selectedSegmentId === "todos") {
-        setSelectedSegmentId(null);
-      } else if (selectedSegmentId) {
-        newUrl += `&segmentId=${selectedSegmentId}`;
+    for (let key in events) {
+      if (events.hasOwnProperty(key)) {
+        const event = events[key];
+        event._embedded?.attractions[0]?.classifications.forEach(
+          (classification) => {
+            genreNamesSet.add(classification.genre?.name);
+          }
+        );
       }
+    }
 
-      await performFetch(newUrl);
-    };
+    console.log("Nombre de genres:", genreNamesSet);
 
-    fetchData();
-  }, [selectedSegmentId, page]);
+    const filteredEvents = events?._embedded?.events.filter((event) => {
+      return event._embedded?.attractions[0]?.classifications.some(
+        (classification) => {
+          return classification.genre?.name === genreName;
+        }
+      );
+    });
+    const restEvents = events?._embedded?.events.filter((event) => {
+      return !filteredEvents.includes(event);
+    });
+    setFilteredEvents(filteredEvents);
+    setRestEvents(restEvents);
+    console.log("Filtered Events: ", filteredEvents);
+    console.log("Rest Events: ", restEvents);
+  }, [index, genreName, events]);
 
-  //Ordena al azar los eventos
-  const shuffledEvents = data?._embedded?.events
-    ? [...data._embedded.events].sort(() => Math.random() * 10 - 5)
-    : [];
-  useEffect(() => {
-    performFetch(url);
-  }, [url]);
+  // const shuffledEvents = data?._embedded?.events
+  //   ? [...data._embedded.events].sort(() => Math.random() * 10 - 5)
+  //   : [];
+
   return (
     <Container className="px-0  mt-5 pt-5">
       <div className="d-flex align-items-center justify-content-between p-0 mb-3">
@@ -62,13 +89,11 @@ export function SportsEvents() {
       </div>
 
       <section className=" row p-0 m-0 d-flex">
-        <PopularSports />
+        <PopularSports handleGenreName={handleGenreName} />
         <section className="px-0 col-12 col-md-8 col-lg-8 d-flex flex-column">
           {isPending && (
             <section
               style={{ minHeight: "23.9rem" }}
-
-
               className="col-12 col-md-6 col-lg-8 mt-5 pt-1  px-0 w-100 d-flex align-items-center justify-content-center "
             >
               <article className="d-flex flex-column w-100 align-items-center">
@@ -83,24 +108,29 @@ export function SportsEvents() {
               </article>
             </section>
           )}
-          {!isPending && data && (
+          {!isPending && events && (
             <>
-              {shuffledEvents.map((event) => (
-                <div className="m-0 p-0 px-0  card__container" key={event.id}>
+              {filteredEvents.length > 0 ? (
+                <div
+                  className="m-0 p-0 px-0  card__container"
+                  key={filteredEvents[index].id}
+                >
                   <div className=" px-0 position-relative d-flex align-items-center card__container">
                     <div className="position-absolute z-2 px-2 w-100 py-2 d-flex flex-column gap-2 card__text ">
-                      <h6 className="m-0 p-0 versalita">{event.name}</h6>
+                      <h6 className="m-0 p-0 versalita">
+                        {filteredEvents[index].name}
+                      </h6>
                       <div className="d-flex align-items-center justify-content-between">
                         <p className="m-0 p-0">
-                          {event.dates?.start?.localDate}
+                          {filteredEvents[index].dates?.start?.localDate}
                         </p>
                         <p className="m-0 p-0">
-                          {event.dates?.start?.localTime}
+                          {filteredEvents[index].dates?.start?.localTime}
                         </p>
                       </div>
                       <div className="d-flex align-items-center justify-content-between">
                         <Link
-                          to={`/description/:${event.id}`}
+                          to={`/description/:${filteredEvents[index].id}`}
                           className="hover"
                         >
                           <button className="btn btn-dark rounded-pill">
@@ -111,22 +141,40 @@ export function SportsEvents() {
                       </div>
                     </div>
                     <div className="px-0">
-                      {event.images && event.images.length > 0 && (
-                        <Image
-                          src={
-                            event.images.find(
-                              (image) =>
-                                image.width === 1024 && image.height === 576
-                            )?.url || event.images[0].url
-                          }
-                          className="rounded-image"
-                          fluid
-                        />
-                      )}
+                      {filteredEvents[index].images &&
+                        filteredEvents[index].images.length > 0 && (
+                          <Image
+                            src={
+                              filteredEvents[index].images.find(
+                                (image) =>
+                                  image.width === 1024 && image.height === 576
+                              )?.url || filteredEvents[index].images[0].url
+                            }
+                            className="rounded-image"
+                            fluid
+                          />
+                        )}
                     </div>
                   </div>
                 </div>
-              ))}
+              ) : (
+                <section className="d-flex flex-column align-items-center justify-content-center mt-5">
+                  <article>
+                    <h3 className="versalita text-white">
+                      No hay eventos cercanos
+                    </h3>
+                  </article>
+                  <article className="d-flex flex-column w-100 align-items-center">
+                    <h2 className="display-4">
+                      {" "}
+                      EVENT
+                      <strong className="fst-italic text__light-green">
+                        PASS
+                      </strong>
+                    </h2>
+                  </article>
+                </section>
+              )}
             </>
           )}
         </section>
