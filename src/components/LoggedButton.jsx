57e6089d9c2955f8f992200bd3a5ce14cd1b2ae5
Marcelo Melogno <marcelomelogno1986@gmail.com>
2023-11-29 03:46:21 -0300

    correccion menor

---
+++ src/components/LoggedButton.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+
+export function LoggedButton(props) {
+  return (
+    <>
+      <button
+        className="btn m-0 p-0 d-flex align-items-center justify-content-center"
+        onClick={props.handleLoggedClick}
+      >
+        {props.isLogged ? (
+          <p className="m-0">Unloged</p>
+        ) : (
+          <p className="m-0">Logged</p>
+        )}
+      </button>
+    </>
+  );
+}
