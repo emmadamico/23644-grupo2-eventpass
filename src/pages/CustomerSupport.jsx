6d76cd1159b1d3750ba7825b32c694f52766e037
CristianAbelRobles <cristianrobles@outlook.com>
2023-12-04 11:52:15 -0300

    se borran imagenes que no se usan y codigo comentado

--- src/pages/CustomerSupport.jsx
+++ src/pages/CustomerSupport.jsx
@@ -89,10 +89,6 @@ export function CustomerSupport() {
                 text: "You must complete the form correctly before submitting it",
             });
         }
-
-
-
-
     };
 
     // const validarBtn = () =>{
@@ -114,73 +110,56 @@ export function CustomerSupport() {
                 <div className="container">
                     <div className="glass-bg rounded rounded-5 overflow-hidden">
                         <div className="row gap-3 gap-lg-0">
-                            <div className="col-lg-6">
+                            <div className="col-lg-6 d-flex align-items-center" style={{backgroundColor: "white"}}>
                                 <img
                                     src="./img/customer_support.jpg"
                                     alt=""
                                     className="img-fluid w-100"
                                 />
                             </div>
-                            <div className="col-lg-6">
+                            <div className="col-lg-6 px-5 pt-3">
                                 <h1 className="text-white versalita">CustomerSupport</h1>
                                 <div className="linea-color mb-3"></div>
                                 <div className="">
-                                <p>
-                                We value your experience with us and want to ensure you have the best possible support. If you have any questions, concerns, or simply need assistance, our dedicated Customer Support team is here for you!
-                                </p>
-                                <form className="d-flex flex-column align-items-center" id="formulario">
-                                    <div className="w-100 mb-3" id="grupo__nombre">
-                                        <label forhtml="nombre" className="form-label">Name</label>
-                                        <input type="text" className="form-control rounded rounded-3" placeholder="User Name" id="nombre" name="nombre" onChange={validarFormulario}/>
-                                        <p className="my-1 p-2 msjError d-none text-bg-danger rouded rounded-1">
-                                            The Name cannot be empty and must be from 4 to 16 characters, numbers or symbols are not allowed.
-                                        </p>
-                                    </div>
-                                    <div className="w-100 mb-3" id="grupo__correo">
-                                        <label forhtml="correo" className="form-label">Email</label>
-                                        <input type="email" className="form-control rounded rounded-3" placeholder="user@mail.com" id="correo" name="correo" onChange={validarFormulario}/>
-                                        <p className="my-1 p-1 msjError d-none text-bg-danger rouded rounded-1">
-                                            The Mail cannot be empty, it can only contain letters, numbers, periods, hyphens and underscores.
-                                        </p>
-                                    </div>
-                                    <div className="w-100 mb-5" id="grupo__consulta">
-                                        <label forhtml="consulta" className="form-label">Question:</label>
-                                        <textarea className="form-control rounded rounded-3" placeholder="Message" id="consulta" name="consulta" onChange={validarFormulario} rows="3"></textarea>
-                                        <p className="my-1 p-1 msjError d-none text-bg-danger rouded rounded-1">
-                                            You must complete the query before you can submit it.
-                                        </p>
-                                    </div>
-                                    <div>
-                                        <button 
-                                            type="submit" 
-                                            className="btn btn__black-green rounded rounded-5 mb-5 px-3"
-                                            onClick={handleSubmit}
-                                            // disabled={btnSubmitDisabled}
-                                        >
-                                            Send <i className="bi bi-send"></i>
-                                        </button>
-                                    </div>
+                                    <p>
+                                    We value your experience with us and want to ensure you have the best possible support. If you have any questions, concerns, or simply need assistance, our dedicated Customer Support team is here for you!
+                                    </p>
+                                    <form className="d-flex flex-column align-items-center" id="formulario">
+                                        <div className="w-100 mb-3" id="grupo__nombre">
+                                            <label forhtml="nombre" className="form-label">Name</label>
+                                            <input type="text" className="form-control rounded rounded-3" placeholder="User Name" id="nombre" name="nombre" onChange={validarFormulario}/>
+                                            <p className="my-1 p-2 msjError d-none text-bg-danger rouded rounded-1">
+                                                The Name cannot be empty and must be from 4 to 16 characters, numbers or symbols are not allowed.
+                                            </p>
+                                        </div>
+                                        <div className="w-100 mb-3" id="grupo__correo">
+                                            <label forhtml="correo" className="form-label">Email</label>
+                                            <input type="email" className="form-control rounded rounded-3" placeholder="user@mail.com" id="correo" name="correo" onChange={validarFormulario}/>
+                                            <p className="my-1 p-1 msjError d-none text-bg-danger rouded rounded-1">
+                                                The Mail cannot be empty, it can only contain letters, numbers, periods, hyphens and underscores.
+                                            </p>
+                                        </div>
+                                        <div className="w-100 mb-5" id="grupo__consulta">
+                                            <label forhtml="consulta" className="form-label">Question:</label>
+                                            <textarea className="form-control rounded rounded-3" placeholder="Message" id="consulta" name="consulta" onChange={validarFormulario} rows="3"></textarea>
+                                            <p className="my-1 p-1 msjError d-none text-bg-danger rouded rounded-1">
+                                                You must complete the query before you can submit it.
+                                            </p>
+                                        </div>
+                                        <div>
+                                            <button 
+                                                type="submit" 
+                                                className="btn btn__black-green rounded rounded-5 mb-5 px-3"
+                                                onClick={handleSubmit}
+                                                // disabled={btnSubmitDisabled}
+                                            >
+                                                Send <i className="bi bi-send"></i>
+                                            </button>
+                                        </div>
+                                        
+                                    </form>
                                     
-                                </form>
-                                
-                            </div>
-
-
-
-
-
-
-
-
-
-
-
-                                {/* <form action="">
-                                    <input type="text" name="" id="" />
-                                    <input type="text" name="" id="" />
-                                    <input type="text" name="" id="" />
-                                    <button type="submit" className="btn btn__black-green rounded rounded-5 mb-5">Send <i className="bi bi-send"></i></button>
-                                </form> */}
+                                </div>
                             </div>
                         </div>
                     </div>
