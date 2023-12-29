def69a7a54c0b6aba2372d45dd4e0c34611451ea
Emmanuel Nenadovit <emmanuelnenadovit@gmail.com>
2023-12-29 01:03:17 -0300

    Added vercel file to backend

--- backend/index.js
+++ backend/index.js
@@ -10,6 +10,8 @@ app.use(express.json());
 
 app.use(cors());
 
+app.get("/", (req, res) => res.status(200).json({ message: "Server is running"}));
+
 app.use("/auth", authRouter);
 
 const PORT = process.env.PORT || 5000;
